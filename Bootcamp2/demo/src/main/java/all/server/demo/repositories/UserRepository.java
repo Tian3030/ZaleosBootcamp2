93f3632e9ee36c68ece894ccc5cf8e533be4c9fa
dap00k <daniel.andres.perez@alumnos.upm.es>
2025-03-14 13:01:31 +0100

    Update UserRepository.java

--- Bootcamp2/demo/src/main/java/all/server/demo/repositories/UserRepository.java
+++ Bootcamp2/demo/src/main/java/all/server/demo/repositories/UserRepository.java
@@ -10,4 +10,6 @@ import java.util.List;
 public interface UserRepository extends JpaRepository<User,String> {
     List<User> findByName(String name);
     List<User> findByHeader(String header);
+    List<User> findByFollowers(Integer followers);
+    List<User> findByFollowed(Integer followed);
 }
