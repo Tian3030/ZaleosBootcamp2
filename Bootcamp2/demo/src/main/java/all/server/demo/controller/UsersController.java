ced384ba73c6df05a0ab5377b0ecd8d86a4b93f9
dap00k <daniel.andres.perez@alumnos.upm.es>
2025-03-17 12:27:49 +0100

    Update UsersController.java

--- Bootcamp2/demo/src/main/java/all/server/demo/controller/UsersController.java
+++ Bootcamp2/demo/src/main/java/all/server/demo/controller/UsersController.java
@@ -1,9 +1,10 @@
 package all.server.demo.controller;
 
-import all.server.demo.repositories.UserRepository;
 import all.server.demo.restobjets.User;
 
+import all.server.demo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,7 +14,7 @@ import java.util.List;
 public class UsersController {
 
     @Autowired
-    private UserRepository userRepo;
+    private UserService userService;
 
     //POST method for the 'users' resource.
     //Takes the users attributes from the request body.
@@ -24,20 +25,7 @@ public class UsersController {
     //      - The name of the user must be at least 4 characters long.
     @PostMapping
     public User createUser(@RequestBody User user) {
-        if (userRepo.existsById(user.getUsername()))
-            throw new IllegalArgumentException("ERROR 400: Username already exists");
-
-        else if (user.getUsername().length() < 4)
-            throw new IllegalArgumentException("ERROR 400: Username too short");
-
-        else if (user.getName().length() < 4)
-            throw new IllegalArgumentException("ERROR 400: Name too short");
-
-        else {
-            user.setFollowers(0);
-            user.setFollowed(0);
-            return userRepo.save(user);
-        }
+        return userService.createUser(user);
     }
 
     //GET method for the 'users' resource.
@@ -46,8 +34,8 @@ public class UsersController {
     //Error Control:
     //      - Cannot search for a non-existent user.
     @GetMapping("/{username}")
-    public User getUser(@PathVariable String username) {
-        return userRepo.findById(username).orElseThrow(() -> new IllegalArgumentException("ERROR 404: User not found"));
+    public User getUser(@PathVariable String username, Model model) {
+        return userService.getUser(username);
     }
 
     //GET method for the 'users' resource.
@@ -58,16 +46,7 @@ public class UsersController {
                                @RequestParam(required = false, defaultValue = "") String header,
                                @RequestParam(required = false) Integer followers,
                                @RequestParam(required = false) Integer followed) {
-        if(!name.isEmpty())
-            return userRepo.findByName(name);
-        else if(!header.isEmpty())
-            return userRepo.findByHeader(header);
-        else if(followers != null)
-            return userRepo.findByFollowers(followers);
-        else if(followed != null)
-            return userRepo.findByFollowed(followed);
-        else
-            return userRepo.findAll();
+        return userService.getUsers(name,header,followers,followed);
     }
 
     //DELETE method for the 'users' resource.
@@ -77,11 +56,7 @@ public class UsersController {
     //      - A non-existent user cannot be deleted.
     @DeleteMapping("/{username}")
     public void deleteUser(@PathVariable String username) {
-
-        if (!userRepo.existsById(username))
-            throw new IllegalArgumentException("ERROR 404: User not found");
-
-        userRepo.deleteById(username);
+        userService.deleteUser(username);
     }
 
     //PUT method for the 'users' resource.
@@ -92,14 +67,7 @@ public class UsersController {
     //      - The id of the user (username) has to be the same in the URL and in the request body.
     @PutMapping("/{username}")
     public User editUser(@PathVariable String username, @RequestBody User userNew) {
-
-        if(!userRepo.existsById(username))
-            throw new IllegalArgumentException("ERROR 404: User not found");
-
-        if(!userNew.getUsername().equals(username))
-            throw new IllegalArgumentException("ERROR 400: Username mismatch");
-
-        return userRepo.save(userNew);
+        return userService.editUser(username,userNew);
     }
 
     //PATCH method for the 'users' resource.
@@ -108,18 +76,7 @@ public class UsersController {
     //Error Control:
     //      - A non-existent user cannot be modified.
     @PatchMapping("/{username}")
-    public User editName (@PathVariable String username, @RequestBody User userNew){
-
-        User userOld = userRepo.findById(username).orElseThrow(() -> new IllegalArgumentException("ERROR 404: User not found"));
-
-        if(userNew.getFollowers() != null) {
-            userOld.setFollowers(userNew.getFollowers());
-        }
-
-        if(userNew.getFollowed() != null) {
-            userOld.setFollowed(userNew.getFollowed());
-        }
-
-        return userRepo.save(userOld);
+    public User patchUser (@PathVariable String username, @RequestBody User userNew){
+        return userService.patchUser(username,userNew);
     }
 }
